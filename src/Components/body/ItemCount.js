f742ef7fb728870c5924fb67346719e6a9048763
Alejo Tomás Clifton Goldney <cliftonalejo@gmail.com>
2021-11-24 16:17:36 -0300

    Desafio N°4 Clase 5

---
+++ src/Components/body/ItemCount.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const ItemCount = ({ getStock, getInitial }) => {
+    const [stock] = useState(getStock);
+    const [count, setCount] = useState(getInitial);
+
+    const handleAdd = () => {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    };
+
+    const handleRemove = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        }
+    };
+
+    const onAdd = () => {
+        window.alert(`¡Agregada la cantidad de ${count} productos a tu carrito!`);
+    };
+
+    return (
+        <div className="itemCount">
+            <p>Camisa Tiger </p>
+            <div>
+                <i className="fas fa-minus" onClick={handleRemove}></i>
+                <span>{count}</span>
+                <i className="fas fa-plus" onClick={handleAdd}></i>
+            </div>
+            <button onClick={onAdd}>Agregar al Carrito</button>
+        </div>
+    );
+};
+
+export default ItemCount;
--- src/Components/body/ItemListContainer.js
+++ src/Components/body/ItemListContainer.js
@@ -1,9 +1,11 @@
 import "./body.scss";
+import ItemCount from './ItemCount';
 
 const ItemListContainer = ({greeting,classItemListContainer}) => {
     return (
         <div className={classItemListContainer}>
             <h1>{greeting}</h1>
+            <ItemCount getStock={10}  getInitial={1}  />
         </div>
     );
 }
