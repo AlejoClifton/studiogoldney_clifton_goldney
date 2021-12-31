c3e7ccf62c88422061c65eca8c9e1c3e8a24333d
Alejo Tomás Clifton Goldney <cliftonalejo@gmail.com>
2021-12-31 20:12:43 -0300

    Cambios Mayores

--- src/Components/Nav/navBarList/cartWidget/CartWidget.js
+++ src/Components/Nav/navBarList/cartWidget/CartWidget.js
@@ -5,12 +5,10 @@ import Context from '../../../../context/CartContext';
 const CartWidget = () => {
     const { getCount } = useContext(Context);
 
-    let count = getCount();
-
     return (
         <Link to={'/cart'} className="CartWidget" >
             <i className="fas fa-shopping-cart"></i>
-            <p>{count}</p>
+            <p>{getCount()}</p>
         </Link>
     );
 };
