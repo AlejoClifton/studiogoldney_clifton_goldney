c04b49a851d7a6510da1259fe1a8b8c5f9b7984b
Alejo Tomás Clifton Goldney <cliftonalejo@gmail.com>
2022-01-03 19:59:36 -0300

    Cambio de .env

--- src/service/firebase/firebase.js
+++ src/service/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBpgo0GgEN7Ayo9f4ghmzbBt9bIsOSeaHQ',
-    authDomain: 'studio-goldney.firebaseapp.com',
-    projectId: 'studio-goldney',
-    storageBucket: 'studio-goldney.appspot.com',
-    messagingSenderId: '1078875871398',
-    appId: '1:1078875871398:web:e572ff92ce82d1ee79fc67',
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
